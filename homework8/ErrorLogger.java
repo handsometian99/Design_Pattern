5fa0afe869ea2ba7c100f0ab2609659e313543a6
handsometian99 <1287834116@qq.com>
2021-06-08 15:02:06 +0800

    责任链模式

---
+++ homework8/ErrorLogger.java
@@ -0,0 +1,12 @@
+package Responsibility;
+
+public class ErrorLogger extends AbstractLogger{
+	public ErrorLogger(int level){
+	     this.level = level;
+	  }
+	 
+	 @Override
+	 protected void write(String message) {    
+	     System.out.println("Error Console::Logger: " + message);
+	  }
+}
---
+++ homework8/FileLogger.java
@@ -0,0 +1,15 @@
+package Responsibility;
+
+public class FileLogger extends AbstractLogger{
+	
+	
+	public FileLogger(int level){
+	      this.level = level;
+	   }
+	 
+	 @Override
+	 protected void write(String message) {    
+	      System.out.println("File::Logger: " + message);
+	   }
+
+}
