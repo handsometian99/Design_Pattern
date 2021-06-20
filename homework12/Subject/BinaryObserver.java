593cf19a3ec6a46e7184b0262496b425d3195b97
handsometian99 <1287834116@qq.com>
2021-06-20 09:11:06 +0800

    观察者模式

---
+++ homework12/Subject/BinaryObserver.java
@@ -0,0 +1,15 @@
+package Subject;
+
+public class BinaryObserver extends Observer{
+ 
+   public BinaryObserver(Subject subject){
+      this.subject = subject;
+      this.subject.attach(this);
+   }
+ 
+   @Override
+   public void update() {
+      System.out.println( "Binary String: " 
+      + Integer.toBinaryString( subject.getState() ) ); 
+   }
+}
---
+++ homework12/Subject/Observer.java
@@ -0,0 +1,6 @@
+package Subject;
+
+public abstract class Observer {
+   protected Subject subject;
+   public abstract void update();
+}
